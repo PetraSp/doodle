d854877af814b2ba25a75f5d2db4da118f8cea72
PetraSpirkova <petra.spirkova@n26.com>
2020-07-28 18:09:12 +0200

    project set-up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component{
+    render() {
+        return(
+            <h1>This is the beginning of the project</h1>
+        );
+    }
+}
+
+export default App;
